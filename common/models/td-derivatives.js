c4c193b082197a5167297228df7812fe9e3aed4a
kishanYuvmedia <kishangopal.yuvmedia@gmail.com>
2023-11-04 09:53:52 +0530

    update cron-job

--- common/models/td-derivatives.js
+++ common/models/td-derivatives.js
@@ -10,8 +10,12 @@ module.exports = function (TdDerivatives) {
   var getIntradayData = app.dataSources.getIntradayData;
   var getOptionExpiry = app.dataSources.getOptionExpiry;
   var getOptionData = app.dataSources.getOptionData;
-  const schedule = "0-59/5 9-14 * * 0-5"; // Replace with your desired cron schedule
-  const scheduletwo = "0-59/30 9-14 * * 0-5";
+  const schedule = "0-59/5 9-16 * * 0-5"; // Replace with your desired cron schedule
+  const scheduletwo = "0-59/30 9-16 * * 0-5";
+  const timeZoneview= {
+    scheduled: true,
+    timezone: "Asia/Kolkata"
+  }
   TdDerivatives.strikeprice = (type, callback) => {
     const currenturl = `${configt.stock.connector}/GetLastQuote/?accessKey=${configt.stock.key}&exchange=NFO&instrumentIdentifier=${type}-I`;
     request(currenturl, function (error, response, body) {
@@ -350,7 +354,7 @@ module.exports = function (TdDerivatives) {
                   putTotal,
                   callTotal,
                   strike,
-                  ...{ time: moment(currentTime).format('HH:mm'),timeUpdate:moment().unix() },
+                  ...{ time: moment().format('HH:mm'),timeUpdate:moment().unix() },
                 };
 
                 if (!_.isEmpty(datatoday)) {
@@ -375,7 +379,7 @@ module.exports = function (TdDerivatives) {
         }
       });
     }
-  });
+  },timeZoneview);
   cron.schedule(scheduletwo, async () => {
     getIntradayData.getProductList((err, response) => {
       if (!_.isEmpty(response)) {
@@ -460,7 +464,7 @@ module.exports = function (TdDerivatives) {
                       putTotal,
                       callTotal,
                       strike,
-                      ...{ time: moment(currentTime).format('HH:mm'),timeUpdate:moment().unix() },
+                      ...{ time: moment().format('HH:mm'),timeUpdate:moment().unix() },
                     };
                     if (!_.isEmpty(datatoday)) {
                       await new Promise((resolve, reject) => {
@@ -486,7 +490,7 @@ module.exports = function (TdDerivatives) {
         }
       }
     })
-  });
+  },timeZoneview);
   
   TdDerivatives.getProductList = (callback) => {
     getIntradayData.getProductList((err, response) => {
