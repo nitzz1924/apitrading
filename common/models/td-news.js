5b726e83301cb5cca085e2f58e43d2cec9e3a69f
kishanYuvmedia <kishangopal.yuvmedia@gmail.com>
2024-02-17 18:05:38 +0530

    data

--- common/models/td-news.js
+++ common/models/td-news.js
@@ -4,27 +4,29 @@ const configt = require("../../server/config.json");
 const app = require("../../server/server");
 const _ = require("lodash");
 module.exports = function (TdNews) {
-    TdNews.getNewsData = (date, type, callback) => {
-        const currenturl = `https://newsapi.org/v2/everything?q=${type}&from=${date}&sortBy=publishedAt&apiKey=d0a15662f3c54133b4821e0a79410ed6`;
-        request(currenturl, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                const jsonData = JSON.parse(body);
-                if (_.isEmpty(body)) {
-                    callback(null, {
-                        StrikePrice: { status: "0", message: "Data not find", Item: [] },
-                    });
-                } else {
-                    callback(null, {
-                        StrikePrice: {
-                            status: "1",
-                            message: "success",
-                            Item: jsonData,
-                        },
-                    });
-                }
+    TdNews.getNewsData = (type, callback) => {
+        const currenturl = `https://newsapi.org/v2/top-headlines?country=in&category=${type}&apiKey=d0a15662f3c54133b4821e0a79410ed6`;
+        const options = {
+            url: currenturl,
+            headers: {
+                'User-Agent': 'tradsathi-api/1.0', // Replace 'YourApp/1.0' with your application name and version
+            },
+        };
+
+        request(options, function (error, response, body) {
+            const jsonData = JSON.parse(body);
+            console.log(JSON.stringify(body));
+            if (_.isEmpty(body)) {
+                callback(null, {
+                    dataList: { status: "0", message: `"Data not find"`, Item: [] },
+                });
             } else {
                 callback(null, {
-                    StrikePrice: { status: "0", message: "Data not find", value: 0 },
+                    dataList: {
+                        status: "1",
+                        message: "success",
+                        Item: jsonData.articles,
+                    },
                 });
             }
         });
