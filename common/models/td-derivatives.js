5a86a6a2836935f314e897a4f71966af69cfb7e0
kishanYuvmedia <kishangopal.yuvmedia@gmail.com>
2025-03-08 10:39:01 +0530

    new api url update

--- common/models/td-derivatives.js
+++ common/models/td-derivatives.js
@@ -11,9 +11,8 @@ module.exports = function (TdDerivatives) {
   var getIntradayData = app.dataSource.getIntradayData;
   var getOptionExpiry = app.dataSource.getOptionExpiry;
   var getOptionData = app.dataSource.getOptionData;
-  
   var scheduletwo = "*/5 4-11 * * 1-5";
-  // var scheduleone = "*/5 10-15 * * 1-5";
+   //var scheduleone = "*/5 10-15 * * 1-5";
   TdDerivatives.strikeprice = (type, callback) => {
     const currenturl = `${configt.stock.connector}/GetLastQuote/?accessKey=${configt.stock.key}&exchange=NFO&instrumentIdentifier=${type}-I`;
     request(currenturl, function (error, response, body) {
@@ -472,117 +471,6 @@ module.exports = function (TdDerivatives) {
       }
     });
   });
-  // cron.schedule(scheduleone, async () => {
-  //   const gettime = getTimeCurrent();
-  //   const listType = ["NIFTY", "BANKNIFTY", "FINNIFTY", "MIDCPNIFTY"];
-  //   for (const type of listType) {
-  //     getIntradayData.getcurrentIntraday(type, (err, response) => {
-  //       if (_.isEmpty(response)) {
-  //         console.log("error 1");
-  //       } else {
-  //         const currentdata = response;
-  //         const strickPrice = response.AVERAGETRADEDPRICE;
-  //         // Wrap the whole operation in a Promise to handle the asynchronous calls
-  //         const processOptionData = async () => {
-  //           try {
-  //             const responsedate = await new Promise((resolve, reject) => {
-  //               getOptionExpiry.getOptionExpiryDates(
-  //                 type,
-  //                 (err, responsedate) => {
-  //                   if (_.isEmpty(responsedate)) {
-  //                     reject("Data not found");
-  //                   } else {
-  //                     resolve(responsedate);
-  //                   }
-  //                 }
-  //               );
-  //             });
-  //             const expirydate = responsedate.EXPIRYDATES[0];
-  //             const responseOption = await new Promise((resolve, reject) => {
-  //               getOptionData.getOptionDataToday(
-  //                 type,
-  //                 expirydate,
-  //                 (err, responseOption) => {
-  //                   if (_.isEmpty(responseOption)) {
-  //                     reject("Data not found");
-  //                   } else {
-  //                     resolve(responseOption);
-  //                   }
-  //                 }
-  //               );
-  //             });
-
-  //             const apiResult = responseOption;
-  //             const putArr = [];
-  //             const callArr = [];
-  //             for (const result of apiResult) {
-  //               const identi = result.INSTRUMENTIDENTIFIER.split("_");
-  //               const value = parseInt(identi[4]);
-  //               if (result.SERVERTIME > 0) {
-  //                 if (identi[3] === "CE") {
-  //                   callArr.push({
-  //                     ...result,
-  //                     value,
-  //                     optionType: identi[3],
-  //                     optionDate: identi[2],
-  //                   });
-  //                 } else if (identi[3] === "PE") {
-  //                   putArr.push({
-  //                     ...result,
-  //                     value,
-  //                     optionType: identi[3],
-  //                     optionDate: identi[2],
-  //                   });
-  //                 }
-  //               }
-  //             }
-  //             const currentOptionStrike = strickPrice;
-  //             const result = findClosestItem(
-  //               callArr,
-  //               currentOptionStrike,
-  //               "value"
-  //             );
-  //             const index = result.index;
-  //             const strike = result.nearestValue;
-  //             if (index !== -1) {
-  //               let putTotal = 0;
-  //               let callTotal = 0;
-  //               for (let i = index - 5; i < index + 5; i++) {
-  //                 putTotal += putArr[i].OPENINTERESTCHANGE;
-  //                 callTotal += callArr[i].OPENINTERESTCHANGE;
-  //               }
-  //               const datatoday = {
-  //                 ...currentdata,
-  //                 putTotal,
-  //                 callTotal,
-  //                 strike,
-  //                 time: gettime,
-  //                 timeUpdate: moment(currentTime).unix(),
-  //               };
-  //               const finalData = indicatorMethods.calculateIndicators(datatoday);
-  //               if (!_.isEmpty(finalData)) {
-  //                 await new Promise((resolve, reject) => {
-  //                   TdDerivatives.create(finalData, (err, data) => {
-  //                     if (err) {
-  //                       console.error(err);
-  //                       reject(err);
-  //                     } else {
-  //                       console.log("Data updated successfully.");
-  //                       resolve();
-  //                     }
-  //                   });
-  //                 });
-  //               }
-  //             }
-  //           } catch (error) {
-  //             console.error("Error:", error);
-  //           }
-  //         };
-  //         processOptionData();
-  //       }
-  //     });
-  //   }
-  // });
   function getTimeCurrent() {
     let date_ob = new Date();
     // Add 5 hours and 30 minutes
