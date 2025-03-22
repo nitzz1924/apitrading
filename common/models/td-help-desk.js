e28bc6d33d77f68d67ead335c80b4e0e953fa8a4
kishanYuvmedia <kishangopal.yuvmedia@gmail.com>
2025-03-22 12:19:14 +0530

    api trading api new stock-data add

--- common/models/td-help-desk.js
+++ common/models/td-help-desk.js
@@ -1,4 +1,6 @@
 "use strict";
 const app = require("../../server/server");
 const _ = require("lodash");
-module.exports = function (TdHelpDesk) {};
+module.exports = function (TdHelpDesk) {
+    
+};
