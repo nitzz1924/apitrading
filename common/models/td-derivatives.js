4fe35d8d02bc0028daf6e3a8c01ca13b06249c8c
kishanYuvmedia <kishangopal.yuvmedia@gmail.com>
2025-05-16 19:38:18 +0530

    stock

--- common/models/td-derivatives.js
+++ common/models/td-derivatives.js
@@ -13,7 +13,8 @@ module.exports = function (TdDerivatives) {
   var getOptionExpiry = app.datasources.getOptionExpiry;
   var getOptionData = app.datasources.getOptionData;
   var schedulew = "30 3 * * *";
-  var scheduletwo = "*/5 4-11 * * 1-5";
+  //var scheduletwo = "*/5 4-11 * * 1-5";
+  var scheduletwo = "*/5 * * * 1-5";
   TdDerivatives.strikeprice = (type, callback) => {
     const currenturl = `${configt.stock.connector}/GetLastQuote/?accessKey=${configt.stock.key}&exchange=NFO&instrumentIdentifier=${type}-I`;
     request(currenturl, function (error, response, body) {
